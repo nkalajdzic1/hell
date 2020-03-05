63d96ad7f689670bd8eb13c20b74488101905ee6
Nadir Kalajdzic <nkalajdzic1@etf.unsa.ba>
2020-03-05 10:16:59 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rs/Main.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  int a,b,c;
+        System.out.println("Unesi brojeve: ");
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+        int zbir=a+b+c;
+        System.out.println("Zbir je " + zbir);
+    }
+}
